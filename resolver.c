4a6787df6eff3735e1e56b27e2f319df54034a3b
Rgokemeijer <Rgokemeijer@sandiego.edu>
2022-04-04 19:28:35 -0700

    Project is complete

--- resolver.c
+++ resolver.c
@@ -1,5 +1,5 @@
 /* Authors: Russell Gokemeijer, Jared Fleiter, Tyler Kreider
-* Date: 04/0
+* Date: 04/04/22
 * This project is an implementation of DNS protocol to build an iterative DNS query resolver.
 *
 *
@@ -25,8 +25,8 @@
 #define MAX_RESPONSE_SIZE 4096
 
 
-char *send_query(char* root_ip, char* hostname, bool is_mx, int sock);
-
+//defines a struct that will be used to parse the flag
+//setion in the header of the response
 struct flag_values{
 	bool response;
 	uint8_t opcode;
@@ -40,18 +40,29 @@ struct flag_values{
 	uint8_t reply_code;
 };
 
+//Stores information that is included in the query
 struct query{
 	char name[173]; //DO NOT CHANGE!!!
 	uint8_t type, class;
 };
 
+//stores answer data
 struct answer{
 	char name[138]; //DO NOT CHANGE!!!
 	uint8_t type, class, data_length;
 	uint32_t ttl;
-	uint8_t extra_data[123]; //NO!!! ugh fine
+	uint8_t extra_data[223]; //NO!!!
 };
 
+
+/**	parses the data we received in the query response
+ * 
+ *
+ * @param response, the entire DNS response
+ * @param offset, The location of the start of the query in the dns response
+ *
+ * @return returns the parsed data put into query_data
+ */
 struct query get_query_data(uint8_t response[], int *offset)
 {
 	struct query query_data;
@@ -64,12 +75,13 @@ struct query get_query_data(uint8_t response[], int *offset)
 
 
 /**
- * 
+ * Get Answer data. Takes in a the DNS message
+ * and parses it into a answer data struct.
  *
- * @param 
- * @param 
+ * @param response, this is the entire DNS message that will be read.
+ * @param offset, this is an int to where the start of this response is.
  *
- * @return 
+ * @return returns the previously defined struct that holds a response.
  */
 struct answer get_answer_data(uint8_t response[], int *offset)
 {
@@ -88,9 +100,11 @@ struct answer get_answer_data(uint8_t response[], int *offset)
 }
 
 /**
- * 
+ * Get flag value data. Takes in an integer and pulls out the different bits.
  *
+ * @param flags, this is the integer that will be broken into its bits
  *
+ * @return returns the previously defined struct that holds all flags.
  */
 struct flag_values get_flag_values(uint16_t flags)
 {
@@ -117,18 +131,21 @@ struct flag_values get_flag_values(uint16_t flags)
 	flags = flags >> 1;
 }
 
+// previous declerations for functions to be used later
+char *send_query(char* root_ip, char* hostname, bool is_mx, int sock);
+char *analyze_request(struct answer *answers, uint8_t *response, int num_answers, char *hostname, bool is_mx, int sock);
+
 /**
- * 
+ *converts the byte data into a string form of the IP address.
  *
  * @param bytes the data we want to convert
  * @param addr the address we want to put converted data into 
  *
- * @return: nothing
+ * @return: no return
  */
 void bytes_to_str(uint8_t* bytes, char* addr){
 	sprintf(addr, "%u.%u.%u.%u", bytes[0], bytes[1], bytes[2], bytes[3]);
 }
-// Note: uint8_t* is a pointer to 8 bits of data.
 
 /**
  * Constructs a DNS query for hostname's Type A record.
@@ -137,7 +154,6 @@ void bytes_to_str(uint8_t* bytes, char* addr){
  * @param hostname The host we are trying to resolve
  * @return The number of bytes in the constructed query.
  */
-
 int construct_query(uint8_t *query, char *hostname, bool is_mx)
 {
 	memset(query, 0, MAX_QUERY_SIZE);
@@ -145,8 +161,8 @@ int construct_query(uint8_t *query, char *hostname, bool is_mx)
 	// first part of the query is a fixed size header
 	DNSHeader *hdr = (DNSHeader*)query;
 
-	// set ID to 5... you should randomize this!
-	hdr->id = htons(5);
+	// generates a random ID to use
+	hdr->id = htons(rand() % 65536);
 
 	// set header flags to request iterative query
 	hdr->flags = htons(0x0000);	
@@ -156,9 +172,6 @@ int construct_query(uint8_t *query, char *hostname, bool is_mx)
 	hdr->a_count=htons(0);
 	hdr->auth_count=htons(0);
 	hdr->other_count=htons(0);
-
-	// We are going to have to wade into pointer arithmetic here since our
-	// struct is a fixed size but our queries will be variably sized.
 
 	// add the name
 	int query_len = sizeof(DNSHeader); 
@@ -171,6 +184,7 @@ int construct_query(uint8_t *query, char *hostname, bool is_mx)
 		*type = htons(1);
 		query_len+=2;
 	}
+	// else sends query type to 15 for mx
 	else{
 		uint16_t *type = (uint16_t*)(query+query_len);
 		*type = htons(15);
@@ -188,12 +202,12 @@ int construct_query(uint8_t *query, char *hostname, bool is_mx)
 /** Searches for the location in the answers struct with a matching DNS record type
  * 
  *
- * @param answers
- * @param type
- * @param num_answers
- * @param response
+ * @param answers, the array of answer structs
+ * @param type, the type of DNS record to search for
+ * @param num_answers, the number of answers in the answers struct
+ * @param response, the dns response to be used for string parsing
  *
- * @return returns an int  which is the location in answers that has the correct DNS type (ns, cname, mx)
+ * @return returns an int which is the location in answers that has the correct DNS type (ns, cname, mx)
  */
 int search_for(struct answer *answers, int type, int num_answers, uint8_t *response){
 	for(int i = 0; i < num_answers; i++){
@@ -258,8 +272,13 @@ char* resolve(char *hostname, bool is_mx) {
 	}
 }
 
-/* Send Query takes in the query and an IP and sends that query looking for a response. 
- * it returns 0 for error and 1 for no error
+/** 
+ * Send Query takes in the query and an IP and sends that query looking for a response. 
+ * @param dest_ip, ip we want to send to
+ * @param hostname, hostname we are trying to resolve
+ * @param is_mx, if it is an mx request or not
+ * @param sock, the socket we are connecting to 
+ * @return returns 0 for error and 1 for no error
 */
 
 char *send_query(char* dest_ip, char* hostname, bool is_mx, int sock){
@@ -301,8 +320,6 @@ char *send_query(char* dest_ip, char* hostname, bool is_mx, int sock){
 	}
 	// The server's response is be located in the response array
 	// Below that data is processed and the needed information is extracted.
-	// Remember that DNS is a binary protocol: if you try printing out response
-	// as a string, it won't work correctly.
 	memset(query, 0, MAX_QUERY_SIZE);
 	DNSHeader *hdr = (DNSHeader*)query;
 	hdr->id = ((uint16_t)response[0] << 8) + response[1];
@@ -313,7 +330,7 @@ char *send_query(char* dest_ip, char* hostname, bool is_mx, int sock){
 	hdr->other_count = ((uint16_t)response[10] << 8) + response[11];
 
 	struct flag_values flag_vals = get_flag_values(hdr->flags);
-	int offset = 12;
+	int offset = 12; // this offset is initial set to 12 to account for flag values and ID
 	struct query queries[hdr->q_count];
 	for(int i = 0; i < hdr->q_count; i++){
 		queries[i] = get_query_data(response, &offset);
@@ -323,9 +340,25 @@ char *send_query(char* dest_ip, char* hostname, bool is_mx, int sock){
 	for (int i = 0; i < num_answers; i++){
 		answers[i] = get_answer_data(response, &offset);
 	};
-// }	
+	return analyze_request(answers, response, num_answers, hostname, is_mx, sock);
+}	
 
-// char *analyze_request(){
+
+/**
+ *  Checks what type of response (ns, cname, mx) and then reads data accordingly using 
+ * an iterative strategy. Returns the address once the correct request type is found.
+ * This function calls itself recursively with new requests.
+ *
+ * @param answers, the list of structs of all of the answers in the response
+ * @param response, The entire DNS response that was received.
+ * @param num_answers, number of responses from request
+ * @param hostname, the hostname we are trying to resolve
+ * @param is_mx, if an mx request or not
+ * @param sock, the socket we are connecting to 
+ *
+ * @return returns a recursive send_query call or an address
+ */
+char *analyze_request(struct answer *answers, uint8_t *response, int num_answers, char *hostname, bool is_mx, int sock){
 	int type = answers[0].type;
 	if (type == 2){ // NS response
 		char new_serv[256]; 
@@ -352,23 +385,26 @@ char *send_query(char* dest_ip, char* hostname, bool is_mx, int sock){
 		printf("Found the NS's IP. Now querying %s to %s\n", hostname, ip);
 		return send_query(ip, hostname, is_mx, sock);
 	}
-	if (type == 1){ // We got the answer
+	if (type == 1){ //type A
 		char *new_addr = malloc(17);
 		bytes_to_str(answers[0].extra_data, new_addr);
 		return new_addr;
 	}
+
 	if (type == 5){ // CNAME
 		char new_name[256];
 		getStringFromDNS(response, answers[0].extra_data, new_name);
 		printf("Was CNAME actual name: %s\n", new_name);
 		return resolve(new_name, is_mx);
 	}
+	//if mx type
 	if (type == 15){
 		char *mx_addr = malloc(172);
 		// Plus two is to skip the preferences filed of mx type responses
 		getStringFromDNS(response,answers[0].extra_data + 2, mx_addr);
 		return mx_addr;
 	}
+	//if SOA type
 	if (type == 6){ 
 		printf("Invalid Hostname.\n");
 		exit(0);
@@ -388,8 +424,8 @@ char *send_query(char* dest_ip, char* hostname, bool is_mx, int sock){
 int main(int argc, char **argv) {
 	printf("\n\n%s, %s, %s\n", argv[0], argv[1], argv[2]);
 	if (argc < 2) {
-		// TODO: provide a more helpful message on how to use the program
 		printf("Invalid program usage for %s!\n", argv[0]);
+		printf("Include the hostname you would like to resolve as an argument.\nIf you would like to make a MX request make the first parameter -m\n");
 		exit(1);
 	}
 	bool not_mx = strcmp("-m", argv[1]);
